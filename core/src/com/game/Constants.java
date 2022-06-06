0161dad948cd447ca466d6b7dbf0192c7081d7b0
Mihai Filip <m.filip@lancaster.ac.uk>
2022-06-06 16:33:20 +0200

    constants added

--- core/src/com/game/Constants.java
+++ core/src/com/game/Constants.java
@@ -1,2 +1,10 @@
-package com.game;public class Constants {
+package com.game;
+
+public class Constants {
+    public static final float GRAVITY = - 9.81f;
+    public static final float PPM = 100f;
+    public static final float WORLD_WIDTH = 1600;
+    public static final float WORLD_HEIGHT = 900;
+
+    public static final String MapPath = "Maps/forest-tileset-lite/Map.tmx";
 }
