29f3c536c375fe2495830e191ca7d12c3d4fbf84
Mihai Filip <m.filip@lancaster.ac.uk>
2022-06-06 16:34:20 +0200

    assets for map creation

---
+++ assets/Maps/forest_tileset_lite/TSX_files/pine2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="pine2" tilewidth="16" tileheight="16" tilecount="516" columns="43">
+ <image source="../Background/pine2.png" width="688" height="199"/>
+</tileset>
